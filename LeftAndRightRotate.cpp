0ea9da8bd3744f3127012e7b0c2a57670f571e40
Pratham-Upasani <prathamupasani75@gmail.com>
2025-10-11 18:08:22 +0530

    Update LeftAndRightRotate.cpp

--- LeftAndRightRotate.cpp
+++ LeftAndRightRotate.cpp
@@ -1,38 +1,47 @@
-//Test
+//Left rotate and Right rotate as many times as you want!
 #include<stdio.h>
 int main(){
-	int a[5]={5,6,4,7,3},i,min=0,max=0,min2=0,max2=0,n=5;
+	int a[5]={2,3,4,5,6},temp,i,n=5;
+	int l,r;
 	
-	for(i=0; i<5; i++){
-		if(a[i]<a[min])
-			min=i;
-	}
+	printf("Enter how many left rotations you want: ");
+	scanf("%d",&l);
 	
-	for(i=0; i<5; i++){
-		if(a[i]>=a[max])
-			max=i;
-	}
-	
-	printf("Elements of array: ");
-	for(i=0; i<5; i++)
+	printf("Array before left rotate: ");
+	for(i=0; i<n; i++){
 		printf("%d ",a[i]);
+	}
+	//Left rotation l times
 	
-	printf("\nMin element = %d",a[min]);
-	printf("\nMax element = %d",a[max]);
-	
-	for(i=0; i<5; i++){
-		if(a[i]<a[max] && a[i]>a[max2]){
-			max2=i;
+	int j;
+	for(j=0; j<l; j++){
+		temp=a[0];
+		for(i=0; i<n; i++){
+			a[i]=a[i+1];
 		}
+		a[n-1]=temp;
 	}
 	
-	for(i=0; i<5; i++){
-		if(a[i]>a[min] && a[i]<a[min2]){
-			min2=i;
-		}
+	
+	printf("\nArray after left rotating %d times: ",l);
+	for(i=0; i<n; i++){
+		printf("%d ",a[i]);
 	}
 	
-	printf("\nMin2 element = %d",a[min2]);
-	printf("\nMax2 element = %d",a[max2]);
+	printf("\n\n\nEnter how many right rotations you want: ");
+	scanf("%d",&r);
 	
+	for(j=0; j<r; j++){
+		temp=a[n-1];
+		for(i=n-1; i>0; i--){
+			a[i]=a[i-1];
+		}
+		a[0]=temp;
+	}
+	
+	printf("\nArray after right rotating %d times: ",r);
+	for(i=0; i<n; i++){
+		printf("%d ",a[i]);
+	}
+	return 0;
 }
